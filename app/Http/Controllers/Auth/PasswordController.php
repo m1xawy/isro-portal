1c5474e547a503f0e78f4cbde63561a1a0de0b88
m1xawy <dev.mixawy@gmail.com>
2023-09-23 09:58:28 +0300

    Fixed Update Password

--- app/Http/Controllers/Auth/PasswordController.php
+++ app/Http/Controllers/Auth/PasswordController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\SRO\Account\TbUser;
+use App\Models\SRO\Portal\MuhAlteredInfo;
+use App\Models\SRO\Portal\MuUser;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Validation\Rules\Password;
 
 class PasswordController extends Controller
@@ -17,8 +21,18 @@ class PasswordController extends Controller
     {
         $validated = $request->validateWithBag('updatePassword', [
             'current_password' => ['required', 'current_password'],
-            'password' => ['required', Password::defaults(), 'confirmed'],
+            'password' => ['required', 'min:6', 'max:32', 'confirmed'],
         ]);
+
+        $UpdateMuUser = MuUser::where('JID', $request->user()->jid)->update(['UserPwd' => md5($request->user()->password)]);
+        $UpdateTbUser = TbUser::where('PortalJID', $request->user()->jid)->update(['password' => md5($request->user()->password)]);
+
+        if (is_null($UpdateMuUser)) {
+            return back()->with('status', 'Error!');
+        }
+        if (is_null($UpdateTbUser)) {
+            return back()->with('status', 'Error!');
+        }
 
         $request->user()->update([
             'password' => Hash::make($validated['password']),
