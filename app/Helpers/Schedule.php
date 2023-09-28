220ae992e4ceffc9ae3d8b644bb6eb89cbb06be9
m1xawy <dev.mixawy@gmail.com>
2023-09-28 22:15:54 +0300

    Fixed Schedule Times (thx @costemania)

--- app/Helpers/Schedule.php
+++ app/Helpers/Schedule.php
@@ -15,7 +15,7 @@ if (!function_exists('getServerTimes')) {
                 "SubInterval_StartTimeMinute",
                 "SubInterval_DurationSecond"))
             ->whereIn("MainInterval_Type", array(1, 3))
-            ->whereIn("ScheduleDefineIdx", array(1, 2, 3, 6, 9, 10, 11, 13, 17, 19, 21, 23))
+            ->whereIn("ScheduleDefineIdx", array(2, 4, 7, 9, 10, 12, 13, 14, 17, 19, 21, 23, 49, 50))
             ->get()
         );
         $now = Carbon\Carbon::now();
@@ -63,18 +63,20 @@ if (!function_exists('getServerTimes')) {
         });
 
         return array(
-            "fortress" => $times->where("id", 6)->min(),
-            "medusa" => $times->where("id", 2)->min(),
-            "roc" => $times->where("id", 1)->min(),
-            "special" => $times->where("id", 3)->min(),
-            "selkis_neith" => $times->where("id", 9)->min(),
-            "anubis_isis" => $times->where("id", 10)->min(),
-            "haroeris_seth" => $times->where("id", 11)->min(),
-            "ctf" => $times->where("id", 13)->min(),
+            "fortress" => $times->where("id", 4)->min(),
+            "medusa" => $times->where("id", 9)->min(),
+            "roc" => $times->where("id", 10)->min(),
+            "special" => $times->where("id", 2)->min(),
+            "selkis_neith" => $times->where("id", 12)->min(),
+            "anubis_isis" => $times->where("id", 13)->min(),
+            "haroeris_seth" => $times->where("id", 14)->min(),
+            "ctf" => $times->where("id", 7)->min(),
             "ba_random" => $times->where("id", 17)->min(),
             "ba_party" => $times->where("id", 19)->min(),
             "ba_guild" => $times->where("id", 21)->min(),
-            "ba_job" => $times->where("id", 23)->min()
+            "ba_job" => $times->where("id", 23)->min(),
+            "survival_solo" => $times->where("id", 50)->min(),
+            "survival_party" => $times->where("id", 49)->min()
         );
     }
 }
